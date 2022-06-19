ea83f9910c10cd3ccd225d800cee4af2df496530
itibanbosi <51027729+itibanbosi@users.noreply.github.com>
2022-06-19 10:55:45 +0900

    更新 eureka_kansui.ts

--- eureka_kansui.ts
+++ eureka_kansui.ts
@@ -23,7 +23,7 @@ namespace newio_blocks {
     }
 
 
-    //% color="#1E90FF" weight=100 block="待ち時間（秒）|%second|" group="0 基本"
+    //% color="#1E90FF" weight=100 block="待ち時間（秒）|%second|" group="1 基本"
     //% second.min=0 second.max=10
     export function driveForwards(second: number): void {
         basic.pause(second * 1000);
@@ -31,7 +31,7 @@ namespace newio_blocks {
 
 
 
-    //% color="#4741f1" weight=80 blockId=kantan1 block="パルス方式センサー値" group="1 センサー簡単ブロック"
+    //% color="#4741f1" weight=80 blockId=kantan1 block="パルス方式センサー値" group="2 センサー簡単ブロック"
     export function kantan1(): number {
     let kansui_V;
     pins.digitalWritePin(DigitalPin.P0, 1);
@@ -45,7 +45,7 @@ namespace newio_blocks {
 
 
 
-    //% color="#4741f1"  weight=78 blockId=kansui_DISP1 block="パルス方式センサーの電圧値を表示" group="1 センサー簡単ブロック"
+    //% color="#4741f1"  weight=78 blockId=kansui_DISP1 block="パルス方式センサーの電圧値を表示" group="2 センサー簡単ブロック"
     export function kansui_DISP1() {
         let kansui_V;
         led.enable(false);
@@ -59,7 +59,7 @@ namespace newio_blocks {
         basic.showNumber(kansui_V);
     }
 
-    //% color="#4741f1" weight=30 block="センサー電圧が |%limit| より |%daisyou| " group="1 センサー簡単ブロック"
+    //% color="#4741f1" weight=30 block="センサー電圧が |%limit| より |%daisyou| " group="2 センサー簡単ブロック"
     export function handan1(limit: number, daisyou: teikou): boolean {
         let kansui_V;
         pins.digitalWritePin(DigitalPin.P0, 1);
@@ -87,7 +87,7 @@ namespace newio_blocks {
     }
 
 
-    //% color="#696969" weight=56 blockId=pump_relay block="水ポンプ |%mode|" group="2 水ポンプ"
+    //% color="#696969" weight=56 blockId=pump_relay block="水ポンプ |%mode|" group="3 水ポンプ"
     export function pump_relay(mode: onoff) {
         switch (mode) {
             case onoff.ON: {
@@ -99,14 +99,14 @@ namespace newio_blocks {
             }
         }
     }
-    //% color="#696969" weight=54 blockId=pump_relay_2 block="水ポンプのアナログ制御|%syuturyoku|" group="2 水ポンプ"
+    //% color="#696969" weight=54 blockId=pump_relay_2 block="水ポンプのアナログ制御|%syuturyoku|" group="3 水ポンプ"
     //% syuturyoku.min=0 syuturyoku.max=1023
     export function IO_relay_2(syuturyoku: number) {
         return pins.analogWritePin(AnalogPin.P2, syuturyoku);
     }
 
 
-    //% color="#ffa800" weight=40 blockId=kansui_onoff block="(simple)センサーへの電圧 |%mode|" group="3 センサー　シンプル"
+    //% color="#ffa800" weight=40 blockId=kansui_onoff block="(simple)センサーへの電圧 |%mode|" group="4 センサー　シンプル"
     export function kansui_onoff(mode: onoff) {
         switch (mode) {
             case onoff.ON:
@@ -119,24 +119,17 @@ namespace newio_blocks {
         }
     }
 
-    //% color="#ffa800" weight=38 blockId=kansui_sokutei block="(simple)センサー電圧値" group="3 センサー　シンプル"
+    //% color="#ffa800" weight=38 blockId=kansui_sokutei block="(simple)センサー電圧値" group="4 センサー　シンプル"
     export function kansui_sokutei(): number {
         return Math.round(pins.analogReadPin(AnalogPin.P1) / 1023 * 33) / 10;
     }
 
-    //% color="#ffa800"  weight=36 blockId=kansui_DISP2 block="(simple)センサーの電圧値を表示" group="3 センサー　シンプル"
+    //% color="#ffa800"  weight=36 blockId=kansui_DISP2 block="(simple)センサーの電圧値を表示" group="4 センサー　シンプル"
     export function kansui_DISP2() {
         let kansui_V;
         kansui_V = Math.round(pins.analogReadPin(AnalogPin.P1) / 1023 * 33) / 10;
         basic.showNumber(kansui_V);
     }
-
-
-
-
-
-
-
 }
 
 
